e2beefca1670db43e772a00689da7601dba79ac0
mahmoudmedhat913 <mahmoudmedhat913>
2024-07-16 16:05:54 +0000

    0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,10 +7,10 @@ def rotate_2d_matrix(matrix):
     """rotate it 90 degree clockwise
     """
     for x, y in enumerate(zip(*reversed(matrix))):
-        matrix[x] = list[y]
+        matrix[x] = list(y)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
     """rotate 2d matrix"""
